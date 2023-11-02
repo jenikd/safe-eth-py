d94feaa97f10328bb4f69957b56457c46682705d
HonzaDajc <jan.dajc@gmail.com>
2023-11-02 17:22:20 +0100

    Add testnet 1.3.0+L2 mastercopy

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -586,6 +586,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.FANTOM_TESTNET: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 4627348, "1.3.0"),
         ("0x5696Ae62C36aF747966522C401FbD57492451f19", 4627348, "1.3.0"),
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 3891238, "1.3.0+L2"),
     ],
     EthereumNetwork.RSK_MAINNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 3891238, "1.3.0+L2"),
